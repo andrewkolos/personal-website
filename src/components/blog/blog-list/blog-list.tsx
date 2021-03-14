19bb2cf8ae302dafb9f532c33917aad85380c3a9
Andrew Kolos <andrewrkolos@gmail.com>
2021-03-14 04:23:14 -0400

    fix blog post ordering bug
    Bug occurred when arriving at Home by following a link within the site (e.g. main title)

--- src/components/blog/blog-list/blog-list.tsx
+++ src/components/blog/blog-list/blog-list.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Styles from "./blog-list.module.scss";
 import SharedStyles from "../../../shared-styles.module.scss";
 
-const BlogList: React.FunctionComponent<{}> = () => {
+const BlogList: React.FunctionComponent<unknown> = () => {
   const data = useStaticQuery(graphql`
     query {
     allMdx {
@@ -25,18 +25,20 @@ const BlogList: React.FunctionComponent<{}> = () => {
   return (
     <div className={SharedStyles.card}>
       <h1>Blog posts</h1>
-      {data.allMdx.edges.reverse().map(({ node }: { node: any }) => (
-        <div className={Styles.container} key={node.id}>
-          <Link className={Styles.titleContainer} to={node.frontmatter.slug}>
-            <h2 className={Styles.title}>{node.frontmatter.title}</h2>
-            <span className={Styles.subtitle}>{node.frontmatter.subtitle}</span>
-          </Link>
-          <p className={Styles.excerpt}>
-            {node.excerpt}&nbsp;
+      {data.allMdx.edges
+        .sort((a: any, b: any) => Date.parse(b.node.frontmatter.date) - Date.parse(a.node.frontmatter.date))
+        .map(({ node }: { node: any }) => (
+          <div className={Styles.container} key={node.id}>
+            <Link className={Styles.titleContainer} to={node.frontmatter.slug}>
+              <h2 className={Styles.title}>{node.frontmatter.title}</h2>
+              <span className={Styles.subtitle}>{node.frontmatter.subtitle}</span>
+            </Link>
+            <p className={Styles.excerpt}>
+              {node.excerpt}&nbsp;
           </p>
-          <p className={Styles.secondaryInfo}>{node.frontmatter.date}</p>
-        </div>
-      ))}
+            <p className={Styles.secondaryInfo}>{node.frontmatter.date}</p>
+          </div>
+        ))}
     </div>
   )
 }
