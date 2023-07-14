4b2b10bcb4383168a4170c880e04fa725179dc60
Andrew Kolos <andrewrkolos@gmail.com>
2023-07-14 16:14:59 -0700

    cap height on image gallery thumbnail

--- pages/art.tsx
+++ pages/art.tsx
@@ -16,7 +16,7 @@ const ReadingList: React.FC<ArtPageProps> = ({ entries }) => {
     <Layout pathName={router.pathname} noSideMargins>
       <div className={SharedStyles.card}>
         <h1>Art</h1>
-        <p>I started to learn to draw in 2023. This page shows some things I have made.</p>
+        <p>I started drawing in 2023. Here is some of my work. Click an image for a better view.</p>
 
         <ArtGallery artEntries={entries} />
       </div>
