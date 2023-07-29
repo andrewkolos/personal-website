4f3bd8925629f24237fa1ecdaa0a49c4561d8dc2
Andrew Kolos <andrewrkolos@gmail.com>
2023-07-29 02:12:10 -0700

    workingtabs

---
+++ components/tabs/tabs.tsx
@@ -0,0 +1,74 @@
+/* eslint-disable react/no-unused-prop-types */
+import React, { PropsWithChildren, ReactElement, useState } from 'react'
+import Styles from './tabs.module.scss'
+import { TabProps } from './tab'
+
+export const Tabs: React.FC<PropsWithChildren> = ({ children }) => {
+  const tabs = asTabs(children)
+
+  ;(() => {
+    const selectedTabs = tabs.filter((td) => td.props.selected)
+    if (selectedTabs.length > 1) {
+      throw Error(
+        `Multiple tabs may not be selected. Selected tabs: ${selectedTabs.map((td) => td.props.title).join(',')}`,
+      )
+    }
+  })()
+
+  const [selectedIndex, setSelectedIndex] = useState<number>(indexOfSelectedTab(tabs))
+
+  return (
+    <>
+      <div className={Styles.tabsContainer}>
+        {tabs.map((t, i) => (
+          <button type="button" className={calcClassNameForTab(i)} onClick={() => setSelectedIndex(i)} key={t.key}>
+            {t.props.title}
+          </button>
+        ))}
+      </div>
+      {tabs.map((t, i) => (
+        <div className={classNameForTabContent(i)} key={t.key}>
+          {t.props.children}
+        </div>
+      ))}
+    </>
+  )
+
+  function calcClassNameForTab(index: number): string {
+    const styles = [Styles.tab]
+    if (selectedIndex === index) {
+      styles.push(Styles.active)
+    }
+    return styles.join(' ')
+  }
+
+  function classNameForTabContent(index: number): string {
+    return index === selectedIndex ? Styles.tabContent : Styles.hidden
+  }
+}
+
+function asTabs(children: React.ReactNode): ReactElement<PropsWithChildren<TabProps>>[] {
+  if (typeof children !== 'object' || children == null) {
+    throw Error()
+  }
+  if (!(Symbol.iterator in children)) {
+    throw Error()
+  }
+
+  const asArray = Array.from(children)
+
+  const validChildren: ReactElement<TabProps>[] = asArray.filter((e) =>
+    React.isValidElement<TabProps>(e),
+  ) as ReactElement<TabProps>[]
+
+  if (validChildren.length < asArray.length) {
+    throw Error('All children of Tabs component must be instances of Tab.')
+  }
+
+  return validChildren
+}
+
+function indexOfSelectedTab(tabs: ReactElement<TabProps>[]): number {
+  const idx = tabs.findIndex((t) => t.props.selected)
+  return idx > -1 ? idx : 0
+}
--- pages/art.tsx
+++ pages/art.tsx
@@ -5,6 +5,8 @@ import Layout from '../components/layout/layout'
 import { ArtEntry, artEntries } from '../lib/art'
 import SharedStyles from '../styles/shared-styles.module.scss'
 import { ArtGallery } from '../components/art/art-gallery/art-gallery'
+import { Tabs } from '../components/tabs/tabs'
+import { Tab } from '../components/tabs/tab'
 
 export interface ArtPageProps {
   entries: ArtEntry[]
@@ -18,7 +20,14 @@ const ReadingList: React.FC<ArtPageProps> = ({ entries }) => {
         <h1>Art</h1>
         <p>I started drawing in 2023. Here is some of my work. Select an image for a better view.</p>
 
-        <ArtGallery artEntries={entries} />
+        <Tabs>
+          <Tab title="Gallery" selected urlSlug="gallery" key="gallery_tab">
+            <ArtGallery artEntries={entries} />
+          </Tab>
+          <Tab title="Scribbles" selected={false} urlSlug="scribbles" key="scribbles_tab">
+            Under construction
+          </Tab>
+        </Tabs>
       </div>
     </Layout>
   )
