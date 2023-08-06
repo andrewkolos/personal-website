032e5ebeed8610237c530e37be684f05e98ea1d8
Andrew Kolos <andrewrkolos@gmail.com>
2023-08-06 01:16:58 -0700

    disable blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,17 @@
 import { useRouter } from 'next/router'
 import React from 'react'
-import { GetStaticProps } from 'next'
-import { BlogPostData } from '../lib/posts/blog-post-data'
-import { getSortedPostsData } from '../lib/posts/get-sorted-posts-data'
 import AboutMe from '../components/about-me/about-me'
-import BlogList from '../components/blog/blog-list/blog-list'
 import Layout from '../components/layout/layout'
 
-interface IndexPageProps {
-  allPostsData: BlogPostData[]
-}
+interface IndexPageProps {}
 
-const IndexPage: React.FC<IndexPageProps> = (props) => {
+const IndexPage: React.FC<IndexPageProps> = () => {
   const router = useRouter()
-  const { allPostsData } = props
   return (
     <Layout pathName={router.pathname}>
       <AboutMe />
-      <BlogList posts={allPostsData} />
     </Layout>
   )
 }
-
-export const getStaticProps: GetStaticProps<IndexPageProps> = async () => ({
-  props: {
-    allPostsData: await getSortedPostsData(),
-  },
-})
 
 export default IndexPage
